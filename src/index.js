83fa4e122c6dab515abfcf5f5ade441e3c41a163
teachat <teachat@aliyun.com>
2019-05-18 22:45:38 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// reducer
+const weight = (state = 160, action) => {
+  switch (action.type) {
+    case 'eat':
+      return state + 10;
+    case 'hungry':
+      return state - 10;
+    default: 
+      return state;
+  }
+}
+
+const store = createStore(weight);
+
+const watch = () => {
+  const current = store.getState();
+  console.log(`三木森 现在体重为${current}`)
+};
+
+// 通过 subscribe 订阅一个函数实现 redux 数据响应
+store.subscribe(watch);
+store.dispatch({type: 'eat'});
+
+store.dispatch({type: 'eat'});
+
+store.dispatch({type: 'eat'});
+
+store.dispatch({type: 'hungry'});
+
+
+// console.log(store.getState());
+
+// store.dispatch({type: 'eat'});
+// console.log('我吃了一些食物');
+// console.log(store.getState());
+
+// console.log('我饿了好几天');
+// store.dispatch({type: 'hungry'});
+// console.log(store.getState());
+
+// console.log('我又饿了好几天');
+// store.dispatch({type: 'hungry'});
+// console.log(store.getState());
+
+/*
+reducer 里面使用switch语句根据传入的类型，输出新的状态
+
+把reducer 传入 createStore(weight)
+
+store.dispatch({ type: 'hungry'})
+
+通过 store.getState() 获取当前的状态
+
+*/
+
+
+
+
